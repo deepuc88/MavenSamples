de046b6376405aa3505c0eb26a712554d954b65d
deepu <deepu@192.168.1.44>
2023-12-22 20:57:58 +0530

    Initial draft

---
+++ Maven_Example/src/main/java/MavenPakage/TestRun.java
@@ -0,0 +1,19 @@
+package MavenPakage;
+
+public class TestRun 
+{
+
+	public void reverse(int a, int b)
+	{
+		int sum = a + b;
+		System.out.println(sum);
+	}
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		TestRun obj = new TestRun();
+		obj.reverse(10,20);
+
+	}
+
+}
