c815e389fbaba8bc9fd190ffbad4ede731530099
deepu <deepu@192.168.1.37>
2024-01-12 21:02:28 +0530

    updated 2024-01-12

--- MavenPrograms/src/main/java/ExcelRead/ReadExcel.java
+++ MavenPrograms/src/main/java/ExcelRead/ReadExcel.java
@@ -18,8 +18,8 @@ public class ReadExcel
 	{
 		f = new FileInputStream("C:\\Deepu\\Study\\Excel\\Employee.xlsx");
 		wb = new XSSFWorkbook(f); //to get the excel work book
-		sh = wb.getSheet("Sheet1"); // to get the particular sheet in the excel work book.
-		//sh = wb.getSheetAt(0);
+		//sh = wb.getSheet("Sheet1"); // to get the particular sheet in the excel work book.
+		sh = wb.getSheetAt(0);
 		Row r = sh.getRow(i); // to get the row in the excel sheet
 		Cell c = r.getCell(j); // to get the cell in the row.
 		return c.getStringCellValue(); // will return the value in the cell
